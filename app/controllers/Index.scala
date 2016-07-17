fadeb2d87c797701821e322bfa8eb401ba3b0478
Ruslan Pilin <dohque@gmail.com>
2016-07-16 22:18:53 -0400

    upgrading all libraries to recen versions

--- app/controllers/Index.scala
+++ app/controllers/Index.scala
@@ -1,14 +1,16 @@
 package controllers
 
+import javax.inject.{ Inject, Singleton }
 import grizzled.slf4j.Logging
 import play.api._
 import play.api.mvc._
 import com.github.tototoshi.play2.scalate._
 
-class Index extends Controller with Logging {
+@Singleton
+class Index @Inject() (scalate: Scalate) extends Controller with Logging {
 
   def index = Action {
-    Ok(Scalate.render("index.jade", Map(
+    Ok(scalate.render("index.jade", Map(
       "title" -> "Landing page"
     )))
   }
